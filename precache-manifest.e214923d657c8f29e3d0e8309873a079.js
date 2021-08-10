a7ea991d4cbe59b4415fc0c572b5b042318951a7
Andrii Milishchuk <andrewmilishchuk@gmail.com>
2021-08-10 19:30:20 +0300

    Updates

---
+++ precache-manifest.e214923d657c8f29e3d0e8309873a079.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de4c7c75c4832e650ff8464be3c0e8bb",
+    "url": "/React_Pokedex_PP/index.html"
+  },
+  {
+    "revision": "ddadcc551eb102f335aa",
+    "url": "/React_Pokedex_PP/static/css/main.012de5c0.chunk.css"
+  },
+  {
+    "revision": "c2a56a6b5924739c8a71",
+    "url": "/React_Pokedex_PP/static/js/2.da7a402a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React_Pokedex_PP/static/js/2.da7a402a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddadcc551eb102f335aa",
+    "url": "/React_Pokedex_PP/static/js/main.d6839502.chunk.js"
+  },
+  {
+    "revision": "528c5722f225339e827d",
+    "url": "/React_Pokedex_PP/static/js/runtime-main.157bad46.js"
+  }
+]);
